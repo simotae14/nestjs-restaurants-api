b9e2b733c9811e721bece58df0509f6a7ee5b29d
Simone Taeggi <startae14@gmail.com>
2025-03-19 23:05:30 +0100

    Creating Restaurant Schema

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
 
+import { RestaurantSchema } from './schemas/restaurant.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Restaurant', schema: RestaurantSchema },
+    ]),
+  ],
   controllers: [RestaurantsController],
-  providers: [RestaurantsService]
+  providers: [RestaurantsService],
 })
 export class RestaurantsModule {}
