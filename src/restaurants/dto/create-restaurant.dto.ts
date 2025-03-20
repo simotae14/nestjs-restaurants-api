78643f6003f13ce508d9fee51c1a5d0a67d647bd
Simone Taeggi <startae14@gmail.com>
2025-03-20 12:33:57 +0100

    Using Globally Validation Pipe and Add Validation to the create restaurant DTO

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,10 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 import { Category } from '../schemas/restaurant.schema';
 
 export class CreateRestaurantDto {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsEmail(
+    {},
+    {
+      message: 'Please enter correct email address',
+    },
+  )
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber('US')
   readonly phoneNo: number;
+
+  @IsNotEmpty()
+  @IsString()
   readonly address: string;
+
+  @IsNotEmpty()
+  @IsEnum(Category, {
+    message: 'Please enter correct Category',
+  })
   readonly category: Category;
 }
