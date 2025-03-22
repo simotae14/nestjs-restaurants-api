fdd8bfff937b58293cda1c63957715083545cee0
Simone Taeggi <startae14@gmail.com>
2025-03-22 19:45:33 +0100

    Hash user password while Sign up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { SignupDTO } from './dto/signup.dto';
+
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -15,10 +20,12 @@ export class AuthService {
   async signUp(signUpDto: SignupDTO): Promise<User> {
     const { name, email, password } = signUpDto;
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await this.userModel.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     return user;
