8c34f1066fdca10851cdc6a69a24bd1b0c9d5700
Simone Taeggi <startae14@gmail.com>
2025-03-22 18:06:00 +0100

    Sign up DTO with Validators

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class SignupDTO {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail(
+    {},
+    {
+      message: 'Please enter correct email address',
+    },
+  )
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  readonly password: string;
+}
