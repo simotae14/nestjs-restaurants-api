0302e8844f3cc1b7df737bbd93e56ec24ee42b9d
Simone Taeggi <startae14@gmail.com>
2025-03-22 19:15:45 +0100

    Add Signup method in Auth controller and Service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignupDTO } from './dto/signup.dto';
+import { User } from './schemas/user.schema';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  // Register a new user
+  @Post('/signup')
+  signUp(@Body() signUpDto: SignupDTO): Promise<User> {
+    return this.authService.signUp(signUpDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { SignupDTO } from './dto/signup.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  // Register User
+  async signUp(signUpDto: SignupDTO): Promise<User> {
+    const { name, email, password } = signUpDto;
+
+    const user = await this.userModel.create({
+      name,
+      email,
+      password,
+    });
+
+    return user;
+  }
+}
