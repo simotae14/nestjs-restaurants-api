b8caffea57c7ea4d9e2545963c6c5e456177d70e
Simone Taeggi <startae14@gmail.com>
2025-03-24 23:49:40 +0100

    Creating User Roles Guard

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+    if (!roles) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    return matchRoles(roles, user.role);
+  }
+}
+
+function matchRoles(roles: string[], userRole: string): boolean {
+  if (!roles.includes(userRole)) return false;
+  return true;
+}
