06d0ca73481285ac896b95fa68e157f066381f15
Simone Taeggi <startae14@gmail.com>
2025-04-01 11:53:30 +0200

    Testing RestaurantService findAll

--- src/restaurants/restaurants.service.spec.ts
+++ src/restaurants/restaurants.service.spec.ts
@@ -1,10 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
 import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './schemas/restaurant.schema';
 
-const mockRestaurantService = {};
+const mockRestaurant = {
+  _id: '67e33754c600340de975a2ee',
+  name: 'Retaurant fast food 2',
+  description: 'This is just a description',
+  email: 'ghulam@gamil.com',
+  phoneNo: 9788246116,
+  address: '200 Olympic Dr, Stafford, VS, 22554',
+  category: 'Fast Food',
+  images: [],
+  location: {
+    type: 'Point',
+    coordinates: [-77.37622, 38.49218],
+    formattedAddress: '200 Olympic Dr, Stafford, VA 22554-7763, US',
+    city: 'Stafford',
+    state: 'VA',
+    zipcode: '22554-7763',
+    country: 'US',
+  },
+  menu: ['67e33778c600340de975a2f3', '67e338acc600340de975a301'],
+  user: '67e1e795f51290a8f81db3b1',
+  createdAt: '2025-03-25T23:08:04.404Z',
+  updatedAt: '2025-03-28T23:10:01.957Z',
+};
+
+const mockRestaurantService = {
+  find: jest.fn(),
+};
 describe('RestaurantsService', () => {
   let service: RestaurantsService;
   let model: Model<Restaurant>;
@@ -26,5 +53,22 @@ describe('RestaurantsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should get all restaurants', async () => {
+      jest.spyOn(model, 'find').mockImplementationOnce(
+        () =>
+          ({
+            limit: () => ({
+              skip: jest.fn().mockResolvedValueOnce([mockRestaurant]),
+            }),
+          }) as any,
+      );
+
+      // call the service method
+      const restaurants = await service.findAll({ keyword: 'restaurant' });
+      expect(restaurants).toEqual([mockRestaurant]);
+    });
   });
 });
